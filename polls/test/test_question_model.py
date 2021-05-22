cfc3990d9b640a0fbf0aef9374db6153fbb60bb9
Dung Nguyen <tandung847@gmail.com>
2021-05-22 13:42:31 +0700

    Django: polls app with admin page of customization

---
+++ polls/test/test_question_model.py
@@ -0,0 +1,14 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_feature_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        feature_question = Question(pub_date=time)
+        self.assertIs(feature_question.was_published_recently(), False)
